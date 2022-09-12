46b35b3f61a1afbc23ed1762ef2d1fab0af81362
JohanDev115 <joarenava@gmail.com>
2022-09-12 17:18:34 -0500

    Bugs fixed

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     const view = `
         <div class="logo">
-            <h1><a href="/" >Home</a></h1>
+            <h1><a href="/RickandMorty" >Home</a></h1>
         </div>
         <nav>
             <a href="#/about/">About</a>
